772f9c1081c70e408125a47485ec4da4554c248d
Navnath Kadam <navnathkadam284@gmail.com>
2025-09-06 12:25:59 +0530

    updated resume

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.js
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true, // this is a must for <Image /> with static export
+  },
+};
 
 export default nextConfig;
