227cf299e3310d0d11466bc080f18a49bb962d7c
Navnath Kadam <navnathkadam284@gmail.com>
2026-04-18 17:01:51 +0530

    feat: changed resume

--- app/layout.js
+++ app/layout.js
@@ -4,13 +4,17 @@ import { ThemeProvider } from "@/context/ThemeContext.js";
 import SnowBackground from "@/components/SnowBackgound";
 import { useContext } from "react";
 import "dotenv/config";
+// import type { Metadata } from "next";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <title>Navnath Kadam | Software Engineer | MERN Stack | AiML Portfolio</title>
-      <meta name="description" content="This is my own Next.js app" />
-      <link rel="icon" href="/myImage.jpg" />
+      <head>
+        <title>Navnath Kadam | Software Engineer | MERN Stack | AiML Portfolio</title>
+        <meta name="description" content="This is my own Next.js app" />
+        <link rel="icon" href="/myImage.jpg" sizes="any" />
+        <link rel="apple-touch-icon" href="/myImage.jpg" />
+      </head>
 
       <body className="relative">
         <ThemeProvider>
